21ab6714ddc45eea95ef7e0b942657bfaae2cf4c
hassan <hassanmohammadi078@gmail.com>
2023-11-07 13:59:05 +0100

    main

---
+++ src/header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+      <header>
+        <h1>Header</h1>
+        <p>header is here</p>
+        <span>_______________</span>
+      </header>
+    );
+  }
+  
+  export default Header;
---
+++ src/main.js
@@ -0,0 +1,12 @@
+function Main() {
+    return (
+      <main>
+        <p>
+            here is the main 
+        </p>
+        <div>_______________</div>
+      </main>
+    );
+  }
+  
+  export default Main;
