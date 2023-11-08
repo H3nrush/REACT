f6623aba8f3ee3b7262fe9155002d83b7da18c6f
H3nrush <138310242+H3nrush@users.noreply.github.com>
2023-11-08 10:19:16 +0100

    Update header.js

--- src/header.js
+++ src/header.js
@@ -1,8 +1,15 @@
+
 function Header() {
+  let userLogin = true ;
     return (
       <header>
         <h1>Header</h1>
         <p>header is here</p>
+        {userLogin ? (
+                            <a href="">enjoy</a>
+                            ):( <button>Please Login</button>
+                        )}
+      <br></br>
         <span>_______________</span>
       </header>
     );
