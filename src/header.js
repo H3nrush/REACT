bd6f56c1c722fd4c2220a849086a3db51f5135bc
H3nrush <138310242+H3nrush@users.noreply.github.com>
2023-11-08 10:40:49 +0100

    Update header.js

--- src/header.js
+++ src/header.js
@@ -1,14 +1,18 @@
 
+
 function Header() {
   let userLogin = true ;
+  const ifPoint = 4;
     return (
       <header>
         <h1>Header</h1>
-        <p>header is here</p>
         {userLogin ? (
-                            <a href="">enjoy</a>
+                            <a href="">Enjoy</a>
                             ):( <button>Please Login</button>
                         )}
+        <p>header is here</p>
+       
+        {ifPoint > 0 ? (<p>User Point is : {ifPoint}</p>) : (<p>0^0</p>) }
       <br></br>
         <span>_______________</span>
       </header>
